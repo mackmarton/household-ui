8916bcc4b9643b819b7692ea5647841d98ca30b9
Marton Mack <mackmarton@edu.bme.hu>
2025-07-29 22:01:22 +0200

    Fixes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -12,6 +12,12 @@ import {
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8081';
 
+// Debug logging to see what URL is being used
+console.log('🔍 API Debug Info:');
+console.log('NEXT_PUBLIC_API_URL:', process.env.NEXT_PUBLIC_API_URL);
+console.log('BASE_URL:', BASE_URL);
+console.log('All env vars:', Object.keys(process.env).filter(key => key.startsWith('NEXT_PUBLIC')));
+
 const api = axios.create({
   baseURL: BASE_URL,
   headers: {
@@ -19,67 +25,83 @@ const api = axios.create({
   },
 });
 
+// Log API requests for debugging
+api.interceptors.request.use(request => {
+  console.log('🚀 API Request:', request.method?.toUpperCase(), request.url);
+  console.log('🔗 Full URL:', `${request.baseURL}${request.url}`);
+  return request;
+});
+
 // Recipe API
 export const recipeApi = {
   getAllRecipes: (): Promise<RecipesResponse> =>
-    api.get('/api/recipes').then(res => res.data),
+    api.get('/recipes').then(res => res.data),
 
   getRecipeById: (id: number): Promise<RecipeDTO> =>
-    api.get(`/api/recipes/${id}`).then(res => res.data),
+    api.get(`/recipes/${id}`).then(res => res.data),
 
   getRecipeIngredients: (recipeId: number): Promise<RecipeIngredientDTO[]> =>
-    api.get(`/api/recipeingredients/${recipeId}`).then(res => res.data),
-
-  updateRecipeIngredient: (id: number, ingredient: RecipeIngredientDTO): Promise<RecipeIngredientDTO> =>
-    api.put(`/api/recipeingredients/${id}`, ingredient).then(res => res.data),
-
-  createRecipeIngredient: (ingredient: Omit<RecipeIngredientDTO, 'id'>): Promise<RecipeIngredientDTO> =>
-    api.post('/api/recipeingredients', ingredient).then(res => res.data),
-
-  deleteRecipeIngredient: (id: number): Promise<void> =>
-    api.delete(`/api/recipeingredients/${id}`).then(() => undefined),
+    api.get(`/recipeingredients/${recipeId}`).then(res => res.data),
 
   createRecipe: (recipe: Omit<RecipeDTO, 'id'>): Promise<RecipeDTO> =>
-    api.post('/api/recipes', recipe).then(res => res.data),
+    api.post('/recipes', recipe).then(res => res.data),
 
   updateRecipe: (id: number, recipe: RecipeDTO): Promise<RecipeDTO> =>
-    api.put(`/api/recipes/${id}`, recipe).then(res => res.data),
+    api.put(`/recipes/${id}`, recipe).then(res => res.data),
 
   deleteRecipe: (id: number): Promise<void> =>
-    api.delete(`/api/recipes/${id}`).then(() => undefined),
+    api.delete(`/recipes/${id}`).then(res => res.data),
+
+  addRecipeIngredient: (recipeIngredient: Omit<RecipeIngredientDTO, 'id'>): Promise<RecipeIngredientDTO> =>
+    api.post('/recipeingredients', recipeIngredient).then(res => res.data),
+
+  updateRecipeIngredient: (id: number, recipeIngredient: RecipeIngredientDTO): Promise<RecipeIngredientDTO> =>
+    api.put(`/recipeingredients/${id}`, recipeIngredient).then(res => res.data),
+
+  deleteRecipeIngredient: (id: number): Promise<void> =>
+    api.delete(`/recipeingredients/${id}`).then(res => res.data),
 };
 
 // Item API
 export const itemApi = {
   getAllItems: (): Promise<ItemsResponse> =>
-    api.get('/api/items').then(res => res.data),
+    api.get('/items').then(res => res.data),
 
   getItemById: (id: number): Promise<ItemDTO> =>
-    api.get(`/api/items/${id}`).then(res => res.data),
+    api.get(`/items/${id}`).then(res => res.data),
 
   createItem: (item: Omit<ItemDTO, 'id'>): Promise<ItemDTO> =>
-    api.post('/api/items', item).then(res => res.data),
+    api.post('/items', item).then(res => res.data),
 
   updateItem: (id: number, item: ItemDTO): Promise<ItemDTO> =>
-    api.put(`/api/items/${id}`, item).then(res => res.data),
+    api.put(`/items/${id}`, item).then(res => res.data),
 
   deleteItem: (id: number): Promise<void> =>
-    api.delete(`/api/items/${id}`).then(() => undefined),
+    api.delete(`/items/${id}`).then(res => res.data),
 };
 
 // Dishwashing API
 export const dishwashingApi = {
-  getAllDishwashingEvents: (): Promise<DishwashingResponse> =>
-    api.get('/api/dishwashing').then(res => res.data),
+  getAllDishwashing: (): Promise<DishwashingResponse> =>
+    api.get('/dishwashing').then(res => res.data),
 
-  getWhoseTurnItIs: (): Promise<WhoseTurnResponse> =>
-    api.get('/api/dishwashing/turn').then(res => res.data),
+  getDishwashingById: (id: number): Promise<DishwashingDTO> =>
+    api.get(`/dishwashing/${id}`).then(res => res.data),
 
-  addDishwashingForMarci: (): Promise<DishwashingDTO> =>
-    api.post('/api/dishwashing/marci').then(res => res.data),
+  createDishwashing: (dishwashing: Omit<DishwashingDTO, 'id'>): Promise<DishwashingDTO> =>
+    api.post('/dishwashing', dishwashing).then(res => res.data),
 
-  addDishwashingForReka: (): Promise<DishwashingDTO> =>
-    api.post('/api/dishwashing/reka').then(res => res.data),
+  updateDishwashing: (id: number, dishwashing: DishwashingDTO): Promise<DishwashingDTO> =>
+    api.put(`/dishwashing/${id}`, dishwashing).then(res => res.data),
+
+  deleteDishwashing: (id: number): Promise<void> =>
+    api.delete(`/dishwashing/${id}`).then(res => res.data),
+
+  getWhoseTurn: (): Promise<WhoseTurnResponse> =>
+    api.get('/dishwashing/whose-turn').then(res => res.data),
+
+  markAsDone: (id: number): Promise<DishwashingDTO> =>
+    api.put(`/dishwashing/${id}/done`).then(res => res.data),
 };
 
 export default api;
